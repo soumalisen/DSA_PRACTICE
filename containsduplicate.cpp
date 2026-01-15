7348d04070603a05399da6369132fd878b05297d
Soumali Sen <soumalisen.bnh@gmail.com>
2026-01-15 21:58:24 +0530

    new commit

---
+++ containsduplicate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+sort(nums.begin(), nums.end());
+
+for (int i = 1; i < nums.size(); ++i) {
+    if (nums[i] == nums[i - 1]) {
+      return true;  
+  }
+}
+    
+return false;  
+}
+
+int main() {
+vector<int> nums = {1, 2, 3, 4, 5, 1};
+bool result = containsDuplicate(nums);
+cout << (result ? "True" : "False") << endl;  
+return 0;
+}
