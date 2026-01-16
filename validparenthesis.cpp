366ff7d42df69f03ffa2d2f66ba24be13f2c85b4
Soumali Sen <soumalisen.bnh@gmail.com>
2026-01-16 20:07:27 +0530

    new commit

---
+++ validparenthesis.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            if (st.empty()) return false;
+
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (isValid(s))
+        cout << "Valid Parentheses\n";
+    else
+        cout << "Invalid Parentheses\n";
+
+    return 0;
+}
