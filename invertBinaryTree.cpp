36fccb9ffb8f79dc89ab14975f8ef9a08bd0fa22
Soumali Sen <soumalisen.bnh@gmail.com>
2026-01-19 22:02:18 +0530

    new commit

---
+++ invertBinaryTree.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+TreeNode* invertTree(TreeNode* root) {
+    if (root == NULL)
+        return NULL;
+
+    TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    invertTree(root->left);
+    invertTree(root->right);
+
+    return root;
+}
+
+void inorder(TreeNode* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+
+    inorder(root);
+    cout << endl;
+
+    invertTree(root);
+
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
