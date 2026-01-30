fb0688075d5e973a328c57de25b86ce08794eaeb
Soumali Sen <soumalisen.bnh@gmail.com>
2026-01-30 21:57:47 +0530

    new

---
+++ sortColors.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else { // nums[mid] == 2
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sortColors(nums);
+
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
